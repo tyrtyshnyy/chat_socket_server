33a35a03c8748404c458bd3af0bf51eebd875549
Sergey Tyrtyshnyy <quality8042@icloud.com>
2023-05-17 21:53:39 +0700

    fix

--- index.ts
+++ index.ts
@@ -14,45 +14,44 @@ const port = 5001;
 
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "*"
+    origin: "*",
   },
 });
 
 //Add this before the app.get() block
 let users: Users[] = [];
-const socketNamespace = socketIO.of("/api");
 
-socketNamespace.on('connection', (socket: Socket) => {
+socketIO.on("connection", (socket: Socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on('message', (data) => {
-    socketNamespace.emit('messageResponse', data);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
   });
-  socket.on('typing', (data) => {socket.broadcast.emit('typingResponse', data);  console.log(data);});
- 
-  
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typingResponse", data);
+    console.log(data);
+  });
+
   //Listens when a new user joins the server
-  socket.on('newUser', (data) => {
+  socket.on("newUser", (data) => {
     //Adds the new user to the list of users
     users.push(data);
-  
+
     //Sends the list of users to the client
-    socketNamespace.emit('newUserResponse', users);
+    socketIO.emit("newUserResponse", users);
   });
 
-  socket.on('disconnect', () => {
-    console.log('🔥: A user disconnected');
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
     //Updates the list of users when a user disconnects from the server
     users = users.filter((user) => user.socketID !== socket.id);
     // console.log(users);
     //Sends the list of users to the client
-    socketNamespace.emit('newUserResponse', users);
+    socketIO.emit("newUserResponse", users);
     socket.disconnect();
   });
 });
 
-
-
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
